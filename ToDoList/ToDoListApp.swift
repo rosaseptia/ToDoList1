f04ee5782eb98dc159bd3a6494d1f9071db2634c
rosa <septiarosalinam@gmail.com>
2022-03-02 10:42:23 +0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Septia Rosalina Malik on 02/03/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
