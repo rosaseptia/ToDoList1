da13529acd81e123bb7713442cfd28f704fe4a16
rosa <septiarosalinam@gmail.com>
2022-03-03 11:31:44 +0700

    learn clean code (done)

--- ToDoList/Models/ItemModel.swift
+++ ToDoList/Models/ItemModel.swift
@@ -7,8 +7,21 @@
 
 import Foundation
 
-struct ItemModel: Identifiable{
-    var id: String = UUID().uuidString
+//immutable struct
+
+struct ItemModel: Identifiable, Codable{
+    var id: String
     let title: String
     let isCompleted : Bool
+    
+    init(id: String = UUID().uuidString, title: String, isCompleted: Bool) {
+        self.id = UUID().uuidString
+        self.title = title
+        self.isCompleted = isCompleted
+    }
+    func updateCompletion() -> ItemModel {
+        return ItemModel(id: id, title: title, isCompleted: !isCompleted)
+    }
 }
+
+
